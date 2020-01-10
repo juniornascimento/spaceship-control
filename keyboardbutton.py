1e7c6b47ffb9280a37db63f2ba7b180f8de63d3b
Samuel Possamai <possamai.sam@gmail.com>
2020-01-09 22:26:18 -0300

    Limited maximum queue size for KeyboardButton

--- keyboardbutton.py
+++ keyboardbutton.py
@@ -5,16 +5,28 @@ from PyQt5.QtWidgets import QPushButton
 
 class KeyboardButton(QPushButton):
 
-    def __init__(self):
+    def __init__(self, can_delete_after=100):
         super().__init__('kb')
 
         super().clicked.connect(self.__clicked)
+
+        self.__key_size = 10
+
         self.__focus = False
         self.__lock = Lock()
         self.__queue = bytearray()
+        self.__can_delete_after = can_delete_after*self.__key_size
 
     def appendToQueue(self, val):
         with self.__lock:
+
+            if len(val) != self.__key_size:
+                raise ValueError('KeyboardButton internal error: '
+                                 'wrong key size')
+
+            if len(self.__queue) > 2*self.__can_delete_after:
+                del self.__queue[0: self.__can_delete_after]
+
             self.__queue.extend(val.encode('utf-8'))
 
     def getAll(self):
