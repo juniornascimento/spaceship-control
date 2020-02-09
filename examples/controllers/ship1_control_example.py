f8c0d646e1573be3b933d7fbebc75ab21248812d
Samuel Possamai <possamai.sam@gmail.com>
2020-02-09 13:28:27 -0300

    Example controller slightly modified

--- examples/controllers/ship1_control_example.py
+++ examples/controllers/ship1_control_example.py
@@ -15,22 +15,48 @@ print('\n\n This is just an example controller')
 
 while True:
     try:
-        if send('1:3: clicked') == '1':
+        if send('1:2: clicked') == '1':
             color_id += 1
             if color_id >= len(colors):
                 color_id = 0
 
-        debug(send('1:4: get'))
+        commands_str = send('1:3: get')
+        commands = []
+        for i in range(10, len(commands_str) + 1, 10):
+            commands.append(commands_str[i - 10: i])
+
+        if '0001000013' in commands:
+            engine_one_intensity = 4
+        else:
+            engine_one_intensity = 0
+
+        if '0001000015' in commands:
+            engine_one_intensity -= 4
+
+        if '0001000012' in commands:
+            engine_two_intensity = 4
+        else:
+            engine_two_intensity = 0
+
+        if '0001000014' in commands:
+            engine_three_intensity = 4
+        else:
+            engine_three_intensity = 0
+
+
+        debug(commands)
 
         debug('Speed:', send('0:3: read'))
 
         pos = ship.position
         angle = ship.angle
-        intensity = -(pos[0] - 500) // 100
-        send(f'0:0: set-property intensity {intensity}')
-        debug(pos, intensity)
+        debug(pos, engine_one_intensity)
         ship.displayPrint(f'<font color={colors[color_id]}>{pos[0]:.1f}, '
                           f'{pos[1]:.1f} ({angle:.1f}º)</font>')
+
+        send(f'0:0: set-property intensity {engine_one_intensity}')
+        send(f'1:0: set-property intensity {engine_two_intensity}')
+        send(f'2:0: set-property intensity {engine_three_intensity}')
 
     except BrokenPipeError:
         break
