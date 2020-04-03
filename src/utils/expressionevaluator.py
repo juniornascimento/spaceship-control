ec5594375e560da71e5d9936321704fcfee20791
Samuel Possamai <possamai.sam@gmail.com>
2020-04-03 02:08:18 -0300

    Adding ExpressionEvaluator

---
+++ src/utils/expressionevaluator.py
@@ -0,0 +1,14 @@
+
+from simpleeval import SimpleEval
+
+class ExpressionEvaluator(SimpleEval):
+
+    def eval(self, expr):
+        self.expr = expr
+
+        expressions = ast.parse(expr.strip()).body
+
+        for i in range(len(expressions) - 1):
+            self.names['_'] = self._eval(expressions[i].value)
+
+        return self._eval(expressions[-1].value)
