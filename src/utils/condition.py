e81c5e050f88ed5a065677e1ff084c6dc9b67b19
Samuel Possamai <possamai.sam@gmail.com>
2020-04-05 08:47:07 -0300

    Using ExpressionEvaluator for Condition

--- src/utils/condition.py
+++ src/utils/condition.py
@@ -1,14 +1,15 @@
 
-from simpleeval import simple_eval
+from .expressionevaluator import ExpressionEvaluator
 
 class Condition:
 
     def __init__(self, expression: str) -> None:
         self.__expr = expression
+        self.__evaluator = ExpressionEvaluator()
 
     def evaluate(self, **kwargs: 'Any') -> 'Any':
 
         try:
-            return bool(simple_eval(self.__expr, names=kwargs))
+            return bool(self.__evaluator.eval(self.__expr, names=kwargs))
         except Exception:
             return False
